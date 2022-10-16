973fd425f5a4a34ab57f14a578d2d3b34943b3d9
Arkadiusz Żmudzin <arek@moodup.team>
2022-10-16 10:49:58 +0200

    feat: add Hello plugin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("1".into()));
+    commands.spawn().insert(Person).insert(Name("2".into()));
+    commands.spawn().insert(Person).insert(Name("3".into()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("Hello {}", name.0);
+    }
+}
+
+fn hello_world() {
+    println!("Hello world!");
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_people)
+            .add_system(hello_world)
+            .add_system(greet_people);
+    }
 }
